15d61c660d936a5d07482175d80e0a7ad592044b
shinazbinshajahan <shinaz2003@gmail.com>
2023-09-24 01:54:09 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './styles/main.css';
+import HomePage from './components/HomePage';
+import AboutPage from './components/AboutPage';
+
+
+function App() {
+  return (
+    <>
+      <HomePage />
+      <AboutPage />
+      <HomePage />
+    </>
+  );
+}
+
+export default App;
